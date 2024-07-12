9c427695e58d8ac4a850e57bd7b314a9acf0f58d
Henry-Cha <sungwon4728@naver.com>
2024-07-12 14:22:32 +0900

    init: create project

---
+++ src/main/java/sesac/docshelper/DocshelperApplication.java
@@ -0,0 +1,13 @@
+package sesac.docshelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocshelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocshelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sesac/docshelper/DocshelperApplicationTests.java
@@ -0,0 +1,13 @@
+package sesac.docshelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocshelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
