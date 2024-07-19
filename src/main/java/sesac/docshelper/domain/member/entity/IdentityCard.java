ebeb8ce1afb0df797af476d331279069186a2158
dalcheonroadhead <wjsaos2081@gmail.com>
2024-07-19 21:17:07 +0900

    fix: fix error about malformed auth code

--- src/main/java/sesac/docshelper/domain/member/entity/IdentityCard.java
+++ src/main/java/sesac/docshelper/domain/member/entity/IdentityCard.java
@@ -1,9 +1,6 @@
 package sesac.docshelper.domain.member.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class IdentityCard {
