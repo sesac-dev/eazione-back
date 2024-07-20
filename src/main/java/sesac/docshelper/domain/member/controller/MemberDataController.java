2925635863362c319e52285c5779f5abfe18a2d2
dalcheonroadhead <wjsaos2081@gmail.com>
2024-07-20 18:02:41 +0900

    feat: Changed the API to return Member entity after adding info

--- src/main/java/sesac/docshelper/domain/member/controller/MemberDataController.java
+++ src/main/java/sesac/docshelper/domain/member/controller/MemberDataController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import sesac.docshelper.domain.member.dto.request.AddInfoRequest;
+import sesac.docshelper.domain.member.entity.Member;
 import sesac.docshelper.domain.member.service.MemberDataService;
 import sesac.docshelper.global.dto.response.ResultResponse;
 import sesac.docshelper.global.util.UserDetailsImpl;
@@ -17,7 +18,7 @@ public class MemberDataController {
     private final MemberDataService memberDataService;
 
     @PostMapping("/addInfo")
-    public ResponseEntity<ResultResponse<String>> addInfo(@RequestBody AddInfoRequest addInfoRequest, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public ResponseEntity<ResultResponse<Member>> addInfo(@RequestBody AddInfoRequest addInfoRequest, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return ResponseEntity.ok(ResultResponse.success(memberDataService.addInfo(userDetails, addInfoRequest)));
     }
 }
--- src/main/java/sesac/docshelper/domain/member/service/MemberDataService.java
+++ src/main/java/sesac/docshelper/domain/member/service/MemberDataService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import sesac.docshelper.domain.member.dto.request.AddInfoRequest;
+import sesac.docshelper.domain.member.entity.Member;
 import sesac.docshelper.domain.member.repository.MemberRepository;
 import sesac.docshelper.global.exception.ErrorCode;
 import sesac.docshelper.global.exception.GlobalException;
@@ -17,20 +18,18 @@ public class MemberDataService {
 
     private final MemberRepository memberRepository;
 
-    public String addInfo(UserDetailsImpl userDetails, AddInfoRequest addInfoRequest) {
+    public Member addInfo(UserDetailsImpl userDetails, AddInfoRequest addInfoRequest) {
         try {
-            memberRepository.findByEmail(userDetails.getEmail())
+            return memberRepository.findByEmail(userDetails.getEmail())
                     .map(member -> {member.setIncome(addInfoRequest.income());
                         member.setHousingType(addInfoRequest.housingType());
                         member.setPhoneNumber(addInfoRequest.phoneNumber());
                         return  member;})
-                    .map(memberRepository::save);
+                    .map(memberRepository::save).orElse(null);
         } catch (Exception e){
             log.error(e.getMessage());
             throw new GlobalException(ErrorCode.ERROR_IN_ADDINFO);
         }
-
-        return  "성공";
     }
 
 }
