65406399cf4c848eb4ca4d2c0424faaf0423b9a0
dalcheonroadhead <wjsaos2081@gmail.com>
2024-08-01 15:53:49 +0900

    feat: get Info in My page

--- src/main/java/sesac/docshelper/domain/member/controller/MemberDataController.java
+++ src/main/java/sesac/docshelper/domain/member/controller/MemberDataController.java
@@ -24,14 +24,20 @@ import sesac.docshelper.global.util.UserDetailsImpl;
 public class MemberDataController {
     private final MemberDataService memberDataService;
 
-    @Operation(description = "사용자 추가 정보 입력하기 (연봉, 주거형태, 전화번호 ")
+    @Operation(description = "사용자 추가 정보 입력하기 ")
     @PostMapping("/add-info")
     public ResponseEntity<ResultResponse<AddInfoResponse>> addInfo(@RequestPart(value = "info") AddInfoRequest addInfoRequest, @RequestPart(value = "img") MultipartFile img, @RequestPart(value = "signature") MultipartFile sign ,@AuthenticationPrincipal UserDetailsImpl userDetails){
         return ResponseEntity.ok(ResultResponse.success(memberDataService.addInfo(
             userDetails, addInfoRequest, img, sign )));
     }
 
-    @Operation(description = "외국인 등록증 여권 읽기, 여권이면 passport, 외국인 등록증 앞: foreginerfront, 뒤: foreginerback")
+    @Operation(description = "사용자 추가 정보 가져오기")
+    @GetMapping("/add-info")
+    public ResponseEntity<ResultResponse<AddInfoResponse>> getInfo(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        return ResponseEntity.ok(ResultResponse.success(memberDataService.getInfo(userDetails)));
+    }
+
+    @Operation(description = "외국인 등록증 or 여권 읽기, 여권이면 passport, 외국인 등록증 앞: foreginerfront, 뒤: foreginerback")
     @PostMapping("/add-ocr/{docsType}")
     public ResponseEntity<ResultResponse<Object>> addIdCardFront(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
@@ -39,6 +45,15 @@ public class MemberDataController {
             @PathVariable(value = "docsType") String docsType
             ){
         return ResponseEntity.ok(ResultResponse.success(memberDataService.addOcrInfo(userDetails,img,docsType)));
+    }
+
+    @Operation(description = "외국인 등록증 or 여권 가져오기 -> docsType: passport, IdCard")
+    @GetMapping("/add-ocr/{docsType}")
+    public ResponseEntity<ResultResponse<Object>> getOcrDocs(
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @PathVariable(value = "docsType") String docsType
+    ){
+        return ResponseEntity.ok(ResultResponse.success(memberDataService.getOcrInfo(userDetails,docsType)));
     }
 
     @Operation(description = "여권 안 들어온 정보 업데이트, DTO 같음")
